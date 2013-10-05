1afc19182b1405cdc6d301a8c9185e7b064c632d
Harvey Rogers <harveyr@gmail.com>
2013-10-05 09:40:31 -0700

    Added OrderedDict

--- ordereddict.py
+++ ordereddict.py
@@ -7,5 +7,10 @@ unsorted = {
     'elastic': 'waist'
 }
 
+# sorted by key
 sorted_ = OrderedDict(sorted(unsorted.items(), key=lambda t: t[0]))
+print('sorted_: {0}'.format(sorted_))
+
+# sorted by value
+sorted_ = OrderedDict(sorted(unsorted.items(), key=lambda t: t[1]))
 print('sorted_: {0}'.format(sorted_))
