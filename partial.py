0f7b5ea17005cfa18edd93a20f3e776eb43b8e28
Harvey Rogers <harveyr@gmail.com>
2013-10-05 10:30:25 -0700

    Added partial.py

---
+++ partial.py
@@ -0,0 +1,42 @@
+from functools import partial
+
+for i in xrange(2, 2):
+    print(i)
+def sing_song(verses, encore=False, *args, **kwargs):
+    for i in xrange(1, verses + 1):
+        if i % 4 == 0:
+            print("Fra la mah")
+        elif i % 3 == 0:
+            print("Kah Nee Tah")
+        elif i % 2 == 0:
+            print("Shish boom bah")
+        else:
+            print("Tra la la")
+    if encore:
+        print("... Freebird!")
+    if len(args) > 0:
+        print("Special thanks to {}!".format(", ".join(args)))
+    if len(kwargs) > 0:
+        print("And we wouldn't be here without the help of our crew:")
+        for name, nickname in kwargs.iteritems():
+            print('\t{} ({})'.format(name, nickname))
+
+sing_song(6)
+print
+
+print("[partial with encore]")
+with_encore = partial(sing_song, encore=True)
+with_encore(2)
+print
+
+print("[partial with extra args and kwargs]")
+
+with_extra_arg = partial(
+    sing_song,
+    3,
+    False,
+    "Tommy",
+    benjamin="The Button",
+    tommy="The Mommy"
+)
+with_extra_arg()
